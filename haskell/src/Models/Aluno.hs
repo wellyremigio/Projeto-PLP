23595954bea2a89cc2f66d41708c4b6c47999711
Welly Remígio <welly.remigio5@gmail.com>
2023-09-14 21:52:32 -0300

    Aluno

--- haskell/src/Models/Aluno.hs
+++ haskell/src/Models/Aluno.hs
@@ -1,1 +1,17 @@
-aldsad
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Model.Aluno where
+
+import Data.Aeson
+import GHC.Generics
+
+data Aluno = Aluno
+  { nome :: String,
+    matricula :: Int,
+    senha :: String
+  }
+  deriving (Show, Generic)
+
+instance FromJSON Aluno
+instance ToJSON Aluno
